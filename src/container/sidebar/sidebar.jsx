67edeb4f607fbc65f0ccff7880e739defc04ec81
ErrazIG <frederic-vml@hotmail.fr>
2024-05-02 18:36:03 +0200

    sidebar

--- src/container/sidebar/sidebar.jsx
+++ src/container/sidebar/sidebar.jsx
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png'
+
 import profile from '../../assets/links/icons8-user-50.png'
 import friends from '../../assets/links/icons8-users-50.png'
 import messages from '../../assets/links/icons8-sms-50.png'
 import games from '../../assets/links/icons8-games-50.png'
 import leaderboards from '../../assets/links/icons8-prize-50.png'
 import tournaments from '../../assets/links/icons8-shield-50.png'
+import help from '../../assets/links/icons8-online-support-50.png'
+import conditions from '../../assets/links/icons8-about-50.png'
+import confidentiality from '../../assets/links/icons8-ask-question-50.png'
+
+import twitter from '../../assets/links/icons8-twitter-50.png'
+import youtube from '../../assets/links/icons8-youtube-50.png'
+import discord from '../../assets/links/icons8-discord-50.png'
+
+
 import style from './sidebar.module.css'
-import { IoMdClose } from "react-icons/io";
 import ThemeToggle from '../../components/theme-toggle/theme-toggle';
 
 const Sidebar = () => {
@@ -17,8 +26,6 @@ const Sidebar = () => {
     return (
         <>
             <nav className={style.sidebar}>
-                <IoMdClose className='closeNav' />
-
                 <Link className={logoLinkClasses} to="/">
                     <img className={style.sidebarLogoImg} src={logo} alt="" />
                     <span>FRD GAMES</span>
@@ -53,24 +60,35 @@ const Sidebar = () => {
                     </ul>
                 </div>
                 <div className={style.navChildContainer}>
-                    <ThemeToggle />
                 </div>
 
                 <div className={style.navChildContainer}>
                     <ul className={style.sidebarLinks}>
-                        <Link className={style.sidebarHelpLink}>
-                            <img src="" alt="" />
-                            <span>Help</span>
+                        <Link className={style.sidebarLink}>
+                            <img className={style.sidebarIcon} src={help} alt="" />
+                            <span className={style.sidebarText}>Help</span>
                         </Link>
-                        <Link className={style.sidebarHelpLink}>
-                            <img src="" alt="" />
-                            <span>Conditions</span>
+                        <Link className={style.sidebarLink}>
+                            <img className={style.sidebarIcon} src={conditions} alt="" />
+                            <span className={style.sidebarText}>Conditions</span>
                         </Link>
-                        <Link className={style.sidebarHelpLink}>
-                            <img src="" alt="" />
-                            <span>Confidentiality</span>
+                        <Link className={style.sidebarLink}>
+                            <img className={style.sidebarIcon} src={confidentiality} alt="" />
+                            <span className={style.sidebarText}>Confidentiality</span>
                         </Link>
                     </ul>
+                    <div className={style.sidebarSocials}>
+                        <Link className={style.sidebarSocial}>
+                            <img className={style.sidebarSocialImg} src={twitter} alt="" />
+                        </Link>
+                        <Link className={style.sidebarSocial}>
+                            <img className={style.sidebarSocialImg} src={youtube} alt="" />
+                        </Link>
+                        <Link className={style.sidebarSocial}>
+                            <img className={style.sidebarSocialImg} src={discord} alt="" />
+                        </Link>
+
+                    </div>
                 </div>
             </nav>
         </>
